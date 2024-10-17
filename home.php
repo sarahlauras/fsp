045a350c5153fd5696a4ffcdd4a7b5853ccf7084
sarahlauras <s160422015@student.ubaya.ac.id>
2024-10-18 01:27:00 +0700

    home

--- home.php
+++ home.php
@@ -12,32 +12,31 @@ $role = $_SESSION["profile"];
 <html lang="en">
 
 <head>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="stylehome.css">
+    <title>Welcome to Esport</title>
+    <link rel="stylesheet" type="text/css" href="stylehomenew.css">
 </head>
 
 <body>
-    <div>Wellcome to</div>
-    <p data-item='Esport'>Esport</p>
-    <section>
-        <div>by Kelompok PASLON</div>
-        <nav>
-            <ul class="menuItems">
-                <li><a href='join_proposal.php' data-item='Join Proposal'>Join Proposal</a></li>
-                <li><a href='achievement.php' data-item='Achievement'>Achievement</a></li>
-                <li><a href='esport_event.php' data-item='Event'>Event</a></li>
+    <div class="welcome-text">Welcome to <span class="bold">Esport</span></div>
+    <div class="subtitle">by Kelompok PASLON</div>
+    <nav>
+        <ul class="menuItems">
+            <li><a href='join_proposal.php'>Join Proposal</a></li>
+            <li><a href='achievement.php'>Achievement</a></li>
+            <li><a href='esport_event.php'>Event</a></li>
 
-                <?php if ($role === 'admin'): ?>
-                    <li><a href='member.php' data-item='Member'>Member</a></li>
-                    <li><a href='team.php' data-item='Team'>Team</a></li>
-                    <li><a href='team_member.php' data-item='Team Members'>Team Members</a></li>
-                    <li><a href='eventteams.php' data-item='Event Teams'>Event Teams</a></li>
-                    <li><a href='esport_game.php' data-item='Game'>Game</a></li>
-                <?php endif; ?>
-            </ul>
-        </nav>
-    </section>
+            <?php if ($role === 'admin'): ?>
+                <li><a href='member.php'>Member</a></li>
+                <li><a href='team.php'>Team</a></li>
+                <li><a href='team_member.php'>Team Members</a></li>
+                <li><a href='eventteams.php'>Event Teams</a></li>
+                <li><a href='esport_game.php'>Game</a></li>
+            <?php endif; ?>
+        </ul>
+    </nav>
 </body>
+
 </html>
