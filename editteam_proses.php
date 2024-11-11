844f29bc85a175013d4c4910da3b5c6e4d7581e1
sarahlauras <s160422015@student.ubaya.ac.id>
2024-11-11 15:40:09 +0700

    upload edit gambar done

--- editteam_proses.php
+++ editteam_proses.php
@@ -16,10 +16,38 @@
             $game = $_POST['game'];
             $idteam = $_POST['idteam'];
             $poster = $_FILES['poster'];
+
+            $target_dir = "teams/";
+            $newPoster = $idteam.".jpg";
+            $target_file = $target_dir.$newPoster;
             
+            //jika ada poster baru
+            if (file_exists($target_file)){
+                if (unlink($target_file)) {
+                    echo "File lama berhasil dihapus.<br>";
+                } else {
+                    echo "Gagal menghapus file lama.<br>";
+                }
+                
+
+                if($poster['name']){
+                    // Validasi file (Hanya JPG yang diizinkan)
+                    $imageFileType = strtolower(pathinfo($poster['name'], PATHINFO_EXTENSION));
+                    if ($imageFileType != "jpg") {
+                        echo "Hanya file JPG yang diizinkan.";
+                        exit();
+                    }
+
+                    if (move_uploaded_file($poster['tmp_name'], $target_file)) {
+                        echo "File baru berhasil diupload.<br>";
+                    } else {
+                        echo "Gagal mengupload file.";
+                        exit();
+                    }
+                }
+            }
             $jumlah = $team->editTeam($name, $game, $idteam);
         }
-        $mysqli->close();
         //setelah semua ter-run, form auto memindahkan user ke halalman insertmovie.php
         header("Location: editteam.php?idteam=$idteam&result=success"); 
     ?>
