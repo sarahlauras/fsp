4348f58c1f13eb2429e6369f636667033d1ff48f
sarahlauras <s160422015@student.ubaya.ac.id>
2024-11-12 11:26:41 +0700

    team

--- deleteteam.php
+++ deleteteam.php
@@ -6,10 +6,14 @@
     // Cek apakah idteam sudah diberikan (misalnya via GET atau POST)
     if(isset($_GET['idteam'])){
         $idteam = $_GET['idteam']; // ambil idteam dari URL
+        $fotoPath = "teams/{$idteam}.jpg";
         // Eksekusi query
         $jumlah = $team->deleteTeam($idteam);
 
         if ($jumlah > 0) {
+            if (file_exists($fotoPath)) {
+                unlink($fotoPath); // Hapus file foto
+            }
             header("Location: team.php?result=success");
             exit();
         } else {
